ca8c2db003752376d457046af956a46daa600c4a
xdiaym <matthew.nekirov@gmail.com>
2021-09-23 18:10:02 +0300

    feat(obj_mod): add `to_string()` method for `builtin_method_object`

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
+#include <objects/builtin_function_object.h>
 #include <objects/int_object.h>
 #include <objects/object.h>
 #include <objects/string_object.h>
-#include <objects/builtin_function_object.h>
 #include <stdio.h>
 
-
 // prints first arg to stdio
-svm_object *print(svm_object** args) {
+svm_object *print(svm_object **args) {
   string_object *str = RETAIN(string_object, svm_object_to_string(args[0]));
   printf("%s", str->data);
   RELEASE(str);
@@ -14,15 +13,14 @@ svm_object *print(svm_object** args) {
 }
 
 int main() {
-  string_object *str =
-      RETAIN(string_object, string_object_from_c_str("Hello world!"));
-  builtin_function_object *print_ = builtin_function_from_c("name", &print);
+  builtin_function_object *print_ =
+      RETAIN(builtin_function_object, builtin_function_from_c("print", &print));
   svm_object_print_debug_info(AS_SVM_OBJECT(print_));
 
-  svm_object *pseudostack[] = {AS_SVM_OBJECT(str)};
+  svm_object *pseudostack[] = {AS_SVM_OBJECT(print_)};
 
   svm_object_call(print_, &pseudostack);
 
-  RELEASE(str);
+  RELEASE(print_);
   return 0;
 }
--- src/objects/builtin_function_object.c
+++ src/objects/builtin_function_object.c
@@ -1,5 +1,6 @@
 #include <objects/builtin_function_object.h>
 #include <objects/object.h>
+#include <objects/string_object.h>
 
 builtin_function_object *builtin_function_from_c(const char *name,
                                                  c_api_function func) {
@@ -15,5 +16,12 @@ static svm_object *builtin_function_call(builtin_function_object *this,
   return this->function(args);
 }
 
+static string_object *
+builtin_function_to_string(builtin_function_object *this) {
+  return string_object_from_c_str(this->name);
+}
+
 svm_object_type builtin_function_object_type = {
-    .m_call = (call_method)&builtin_function_call};
+    .m_call = (call_method)&builtin_function_call,
+    .m_to_string = (to_string_method)&builtin_function_to_string,
+};
