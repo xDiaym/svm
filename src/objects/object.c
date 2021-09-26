4b77bcac8e6c64c0af5815baa4c3734fd587a63a
xdiaym <matthew.nekirov@gmail.com>
2021-09-26 11:46:11 +0300

    fix(obj_mod): fix first object circular reference

--- src/objects/object.c
+++ src/objects/object.c
@@ -11,9 +11,10 @@ static svm_object *last_object = NULL;
 static void update_global_objects_pointers(svm_object *obj) {
   if (first_object == NULL) {
     first_object = last_object = obj;
+  } else {
+    last_object->next = obj;
+    last_object = obj;
   }
-  last_object->next = obj;
-  last_object = obj;
 }
 
 static void svm_object_print_type(svm_object_type *type) {
